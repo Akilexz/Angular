be4a6decc38bcf70a49262a22ffa003610f8ff03
unknown <joel.castro.espinosa.1i@gmail.com>
2018-10-31 11:18:49 -0500

    Listo

---
+++ src/tipoPublicaciones1/tipoPublicaciones.module1.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { CreateComponent } from './create/create.component';
+import { DeleteComponent } from './delete/delete.component';
+import { FieldsetModule } from 'primeng/fieldset';
+import { ToastModule } from 'primeng/toast';
+import { InputMaskModule } from 'primeng/inputmask';
+import { CalendarModule } from 'primeng/calendar';
+import { PaginatorModule } from 'primeng/paginator';
+
+
+@NgModule({
+    imports: [
+        CommonModule,
+        RouterModule,
+        ToastModule,
+        InputMaskModule,
+        CalendarModule,
+        PaginatorModule,
+        FieldsetModule
+    ],
+    declarations: [
+        CreateComponent,
+        DeleteComponent
+    ],
+    exports: [
+        CreateComponent,
+        DeleteComponent
+    ]
+})
+export class PublicacionesModule { }
